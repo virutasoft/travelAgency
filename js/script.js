82f7cf9e0635943822e8fb461a353cebbd661315
virutasoft <virutasoft@gmail.com>
2021-12-18 18:32:48 -0500

    Search form finished html/css/js(only appearance)

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
 };
 
+// SEARCH FORM
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.add('active');
+};
+
+document.querySelector('#close-search').onclick = () =>{
+    searchForm.classList.remove('active');
+};
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+};
