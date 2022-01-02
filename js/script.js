48b9cc3a06cb45d9d42d221501fb18ef1f72ee9d
virutasoft <virutasoft@gmail.com>
2022-01-02 17:02:09 -0500

    Blog section finished

--- js/script.js
+++ js/script.js
@@ -125,3 +125,34 @@ breakpoints: {
   },
 });
 // reviews slider
+
+// blogs slider
+var swiper = new Swiper(".blogs-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+breakpoints: {
+    0: {
+        slidesPerView: 1,
+  
+    },
+    768: {
+      slidesPerView: 2,
+
+    },
+    991: {
+      slidesPerView: 3,
+
+    },
+    
+  },
+});
+// blogs slider
