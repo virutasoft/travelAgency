1f2d9906834a16b2d323e209fa589ead9588ddba
virutasoft <virutasoft@gmail.com>
2022-01-02 15:48:15 -0500

    Reviews section finished

--- js/script.js
+++ js/script.js
@@ -94,3 +94,34 @@ breakpoints: {
   },
 });
 //product slider
+
+// reviews slider
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+breakpoints: {
+    0: {
+        slidesPerView: 1,
+  
+    },
+    640: {
+      slidesPerView: 2,
+
+    },
+    768: {
+      slidesPerView: 3,
+
+    },
+    
+  },
+});
+// reviews slider
